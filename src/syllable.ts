0afeac2a96378efacdec695e764f28be03cc89af
Charles Loder <charlesloder@Charless-MacBook-Pro.local>
2020-09-01 07:54:02 -0400

    consistent getter for syllables

--- src/syllable.ts
+++ src/syllable.ts
@@ -2,17 +2,19 @@ import { Cluster } from "./cluster";
 
 export class Syllable {
   clusters: Cluster[];
-  text: string;
   isClosed: boolean;
   isAccented: boolean;
   isFinal: boolean;
 
   constructor(clusters: Cluster[], { isClosed = false, isAccented = false, isFinal = false } = {}) {
     this.clusters = clusters;
-    this.text = clusters.reduce((init, cluster) => init + cluster.text, "");
     this.isClosed = isClosed;
     this.isAccented = isAccented;
     this.isFinal = isFinal;
+  }
+
+  get text() {
+    return this.clusters.reduce((init, cluster) => init + cluster.text, "");
   }
 
   get chars() {
