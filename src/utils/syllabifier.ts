4e9b4e662fefbe106785c2633920353030c86885
Charles Loder <charlesloder@Charless-MacBook-Pro.local>
2020-09-08 07:06:56 -0400

    improved syllabification

--- src/utils/syllabifier.ts
+++ src/utils/syllabifier.ts
@@ -16,65 +16,133 @@ export const syllabify = (text: string) => {
     let syl: Cluster[] = [];
 
     clusters.forEach((cluster) => {
+      //  A. Does the cluster have a vowel? YES
       if (cluster.hasVowel) {
+        // A.1 Is there already a vowel present? NO
         if (!vowelPresent) {
           syl.push(cluster);
-          vowelPresent = !vowelPresent;
+          vowelPresent = true;
           currVowelShort = cluster.hasShortVowel;
           isClosed = false;
-        } else {
-          const dageshRegx = /\u{05BC}/u;
-          isClosed = dageshRegx.test(cluster.text);
-          isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
-          syllables.push(new Syllable(syl, { isClosed, isAccented }));
-          syl = [];
-          syl.push(cluster);
-          isClosed = false;
-          currVowelShort = cluster.hasShortVowel;
         }
-      } else if (cluster.hasHalfVowel) {
-        if (syl.length) {
-          isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
-          syllables.push(new Syllable(syl, { isClosed, isAccented }));
-          syl = [];
-          vowelPresent = !vowelPresent;
-          currVowelShort = cluster.hasShortVowel;
-        }
-        syl.push(cluster);
-      } else if (cluster.hasShewa) {
-        if (!vowelPresent) {
-          syl.push(cluster);
-        } else if (vowelPresent) {
-          if (currVowelShort) {
-            isClosed = !isClosed;
-            syl.push(cluster);
-            let isFinal = /תְּ|ךְ/.test(cluster.text);
+        // A.2 Is there already a vowel present? YES
+        else {
+          // A.2.a is the cluster a vav + holem? YES
+          const holemVav = /וֹ/;
+          if (holemVav.test(cluster.text)) {
+            const pop: Cluster | undefined = syl.length ? syl.pop() : undefined;
             isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
-            syllables.push(new Syllable(syl, { isClosed, isAccented, isFinal }));
+            syllables.push(new Syllable(syl, { isClosed: false, isAccented: isAccented }));
             syl = [];
-            vowelPresent = !vowelPresent;
-            currVowelShort = cluster.hasShortVowel;
-          } else {
-            syllables.push(new Syllable(syl, { isClosed, isAccented }));
-            isClosed = !isClosed;
-            syl = [];
+            if (pop) syl.push(pop);
             syl.push(cluster);
-            vowelPresent = !vowelPresent;
-            currVowelShort = cluster.hasShortVowel;
             isClosed = false;
           }
-        }
-      } else {
-        const materRegx = /[הוי]/;
-        if (materRegx.test(cluster.text)) {
-          isClosed = false;
-          syl.push(cluster);
-        } else {
-          syl.push(cluster);
-          isClosed = true;
+          // A.2.a is the cluster a vav + holem? NO
+          else {
+            const dageshRegx = /\u{05BC}/u;
+            isClosed = dageshRegx.test(cluster.text);
+            isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+            syllables.push(new Syllable(syl, { isClosed, isAccented }));
+            syl = [];
+            syl.push(cluster);
+            isClosed = false;
+            currVowelShort = cluster.hasShortVowel;
+          }
         }
       }
-    }); // forEach
+      // B. Does the cluster have a vowel? NO
+      else {
+        // B.1 Does the cluster have a shewa? YES
+        if (cluster.hasShewa) {
+          // B.1.b Is there a vowel present already? NO
+          if (!vowelPresent) {
+            syllables.push(new Syllable([cluster], { isClosed: false, isAccented: false, isFinal: false }));
+          }
+          // B.1.a Is there a vowel present already? YES
+          else {
+            // B.1.a.iii Is it a final form? YES
+            if (/תְּ|ךְ/.test(cluster.text)) {
+              syl.push(cluster);
+              isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+              isClosed = true;
+            }
+            // B.1.a.i Is the present vowel a short vowel? YES
+            else if (currVowelShort) {
+              const dageshRegx = /\u{05BC}/u;
+              // B.1.a.i.A Does the cluster have a doubling dagesh? YES
+              if (dageshRegx.test(cluster.text)) {
+                isClosed = true;
+                isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+                syllables.push(new Syllable(syl, { isClosed: isClosed, isAccented: isAccented }));
+                syl = [];
+                syl.push(cluster);
+                vowelPresent = true;
+              }
+              // B.1.a.ii.B Does the cluster have a doubling dagesh? NO
+              else {
+                syl.push(cluster);
+                isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+                syllables.push(new Syllable(syl, { isClosed: true, isAccented: isAccented }));
+                syl = [];
+                currVowelShort = false;
+                vowelPresent = false;
+              }
+            }
+            // B.1.a.ii Is the present vowel a short vowel? NO
+            else {
+              isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+              syllables.push(new Syllable(syl, { isClosed: false, isAccented: isAccented }));
+              syl = [];
+              syl.push(cluster);
+              vowelPresent = true;
+            }
+          }
+        }
+        // B.2 Does the cluster have a shewa? NO
+        else {
+          // B.2.a Is a vowel present? YES
+          if (vowelPresent) {
+            const nonVowelmaterRegx = /[הא]/;
+            const dageshRegx = /\u{05BC}/u;
+            // B.2.a.i Is it a final-he or aleph
+            if (nonVowelmaterRegx.test(cluster.text)) {
+              // B.2.a.i.A&B does he have a mappiq?
+              syl.push(cluster);
+              isClosed = dageshRegx.test(cluster.text);
+            }
+            // B.2.a.ii it is a yod or vav?
+            else {
+              const vowelmaterRegx = /[יו]/;
+              const shureqRegx = /וּ/;
+              // B.2.a.ii.I it is a shureq? YES
+              if (shureqRegx.test(cluster.text)) {
+                const pop: Cluster | undefined = syl.length ? syl.pop() : undefined;
+                isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
+                syllables.push(new Syllable(syl, { isClosed: false, isAccented: isAccented }));
+                syl = [];
+                if (pop) syl.push(pop);
+                syl.push(cluster);
+                isClosed = false;
+              }
+              // B.2.a.ii.I it is a shureq? NO
+              else {
+                syl.push(cluster);
+                isClosed = !vowelmaterRegx.test(cluster.text);
+              }
+            }
+          }
+          // B.2.b Is a vowel present? NO
+          else {
+            // it could be a shureq or a consonant waiting for a mater
+            const shureqRegx = /וּ/;
+            syl.push(cluster);
+            vowelPresent = shureqRegx.test(cluster.text);
+          }
+        }
+      }
+    });
+
     if (syl.length) {
       isAccented = syl.filter((cluster) => cluster.hasTaamei).length ? true : false;
       syllables.push(new Syllable(syl, { isClosed, isAccented, isFinal: true }));
--- test/syllables.test.ts
+++ test/syllables.test.ts
@@ -21,36 +21,42 @@ const tests = (original: string, sylArr: string[], closedArr: boolean[], accentA
 };
 
 describe.each`
-  description                                        | original  | sylArr      | closedArr  | accentArr
-  ${"1 Syl: lexical form - one vowel, closed (yām)"} | ${"יָ֥ם"} | ${["יָ֥ם"]} | ${[true]}  | ${[true]}
-  ${"1 Syl: inflected preposition, closed (lāk)"}    | ${"לֵךְ"} | ${["לָךְ"]} | ${[true]}  | ${[true]}
-  ${"1 Syl: inflected preposition, open (lô)"}       | ${"ל֣וֹ"} | ${["ל֣וֹ"]} | ${[false]} | ${[true]}
+  description                                        | original   | sylArr       | closedArr  | accentArr
+  ${"1 Syl: lexical form - one vowel, closed (yām)"} | ${"יָ֥ם"}  | ${["יָ֥ם"]}  | ${[true]}  | ${[true]}
+  ${"1 Syl: final kaph (lāk)"}                       | ${"לָ֛ךְ"} | ${["לָ֛ךְ"]} | ${[true]}  | ${[true]}
+  ${"1 Syl: with a mater (lô)"}                      | ${"ל֣וֹ"}  | ${["ל֣וֹ"]}  | ${[false]} | ${[true]}
 `("$description", ({ original, sylArr, closedArr, accentArr }) => {
   tests(original, sylArr, closedArr, accentArr);
 });
 
 describe.each`
-  description                                                                 | original           | sylArr                       | closedArr               | accentArr
-  ${"2 Syl: lexical form - two vowels (dābār)"}                               | ${"דָּבָ֑ר"}       | ${["דָּ", "בָ֑ר"]}           | ${[false, true]}        | ${[false, true]}
-  ${"2 Syl: lexical form - two vowels (qodeš), accent on first"}              | ${"קֹ֔דֶשׁ"}       | ${["קֹ֔", "דֶשׁ"]}           | ${[false, true]}        | ${[true, false]}
-  ${"2 Syl: lexical form - two vowels, final shewa (melek), accent on first"} | ${"מֶ֣לֶךְ"}       | ${["מֶ֣", "לֶךְ"]}           | ${[false, true]}        | ${[true, false]}
-  ${"2 Syl: lexical form (dāwid) prefixed conj w/ shewa"}                     | ${"וְדָוִ֖ד"}      | ${["וְדָ", "וִ֖ד"]}          | ${[false, true]}        | ${[false, true]}
-  ${"2 Syl: lexical form contains hatef (ĕlohim)"}                            | ${"אֱלֹהִ֑ים"}     | ${["אֱלֹ", "הִ֑ים"]}         | ${[false, true]}        | ${[false, true]}
-  ${"2 Syl: lexical form contains hatef (ĕmet) prefixed conj w/ vowel"}       | ${"וֶאֱמֶ֔ת"}      | ${["וֶ", "אֱמֶ֔ת"]}          | ${[false, true]}        | ${[false, true]}
-  ${"2 Syl: lexical form one shewa and closes syllable (midbar)"}             | ${"מִדְבַּ֣ר"}     | ${["מִדְ", "בַּ֣ר"]}         | ${[true, true]}         | ${[false, true]}
-  ${"2 Syl: inflected form with one shewa and begins syllable (bārǝkî)"}      | ${"בָּרְכ֣וּ"}     | ${["בָּ", "רְכ֣וּ"]}         | ${[false, false]}       | ${[false, true]}
-  ${"2 Syl: with qamets qatan (ḥokmâ)"}                                       | ${"חָכְמָ֑ה"}      | ${["ח\u{5C7}כְ", "מָ֑ה"]}    | ${[true, false]}        | ${[false, true]}
-  ${"2 Syl: with qamets gadol (ḥākǝmâ)"}                                      | ${"חָֽכְמָ֖ה"}     | ${["חָֽ", "כְמָ֖ה"]}         | ${[false, false]}       | ${[false, true]}
-  ${"3 Syl: 2ms qatal verb consecution (wǝšāmartā́)"}                          | ${"וְשָׁמַרְתָּ֖"} | ${["וְשָׁ", "מַרְ", "תָּ֖"]} | ${[false, true, false]} | ${[false, false, true]}
-  ${"3 Syl: lexical form - two vowels (dābār) + article"}                     | ${"הַדָּבָ֥ר"}     | ${["הַ", "דָּ", "בָ֥ר"]}     | ${[true, false, true]}  | ${[false, false, true]}
+  description                                                                 | original       | sylArr                    | closedArr         | accentArr
+  ${"2 Syl: lexical form - two vowels (dābār)"}                               | ${"דָּבָ֑ר"}   | ${["דָּ", "בָ֑ר"]}        | ${[false, true]}  | ${[false, true]}
+  ${"2 Syl: lexical form - two vowels (qodeš), accent on first"}              | ${"קֹ֔דֶשׁ"}   | ${["קֹ֔", "דֶשׁ"]}        | ${[false, true]}  | ${[true, false]}
+  ${"2 Syl: lexical form - two vowels, final shewa (melek), accent on first"} | ${"מֶ֣לֶךְ"}   | ${["מֶ֣", "לֶךְ"]}        | ${[false, true]}  | ${[true, false]}
+  ${"2 Syl: lexical form contains hatef (ănî)"}                               | ${"אֲנִ֥י"}    | ${["אֲ", "נִ֥י"]}         | ${[false, false]} | ${[false, true]}
+  ${"2 Syl: lexical form one shewa and closes syllable (midbar)"}             | ${"מִדְבַּ֣ר"} | ${["מִדְ", "בַּ֣ר"]}      | ${[true, true]}   | ${[false, true]}
+  ${"2 Syl: with qamets qatan (ḥokmâ)"}                                       | ${"חָכְמָ֑ה"}  | ${["ח\u{5C7}כְ", "מָ֑ה"]} | ${[true, false]}  | ${[false, true]}
 `("$description", ({ original, sylArr, closedArr, accentArr }) => {
   tests(original, sylArr, closedArr, accentArr);
 });
 
 describe.each`
-  description                                             | original           | sylArr                       | closedArr               | accentArr
-  ${"3 Syl: 2ms qatal verb consecution (wǝšāmartā́)"}      | ${"וְשָׁמַרְתָּ֖"} | ${["וְשָׁ", "מַרְ", "תָּ֖"]} | ${[false, true, false]} | ${[false, false, true]}
-  ${"3 Syl: lexical form - two vowels (dābār) + article"} | ${"הַדָּבָ֥ר"}     | ${["הַ", "דָּ", "בָ֥ר"]}     | ${[true, false, true]}  | ${[false, false, true]}
+  description                                                               | original       | sylArr                   | closedArr                | accentArr
+  ${"3 Syl: lexical form contains hatef (ĕlohim)"}                          | ${"אֱלֹהִ֑ים"} | ${["אֱ", "לֹ", "הִ֑ים"]} | ${[false, false, true]}  | ${[false, false, true]}
+  ${"3 Syl: lexical form (dāwid) prefixed conj w/ shewa"}                   | ${"וְדָוִ֖ד"}  | ${["וְ", "דָ", "וִ֖ד"]}  | ${[false, false, true]}  | ${[false, false, true]}
+  ${"3 Syl: lexical form contains hatef (ĕmet) prefixed conj w/ vowel"}     | ${"וֶאֱמֶ֔ת"}  | ${["וֶ", "אֱ", "מֶ֔ת"]}  | ${[false, false, true]}  | ${[false, false, true]}
+  ${"3 Syl: inflected form with medial vocal shewa (bārǝkî)"}               | ${"בָּרְכ֣וּ"} | ${["בָּ", "רְ", "כ֣וּ"]} | ${[false, false, false]} | ${[false, false, true]}
+  ${"3 Syl: inflected form with medial vocal shewa and doubling (sappǝrû)"} | ${"סַפְּר֤וּ"} | ${["סַ", "פְּ", "ר֤וּ"]} | ${[true, false, false]}  | ${[false, false, true]}
+  ${"3 Syl: with qamets gadol (ḥākǝmâ)"}                                    | ${"חָֽכְמָ֖ה"} | ${["חָֽ", "כְ", "מָ֖ה"]} | ${[false, false, false]} | ${[false, false, true]}
+  ${"3 Syl: lexical form - two vowels (dābār) + article"}                   | ${"הַדָּבָ֥ר"} | ${["הַ", "דָּ", "בָ֥ר"]} | ${[true, false, true]}   | ${[false, false, true]}
+`("$description", ({ original, sylArr, closedArr, accentArr }) => {
+  tests(original, sylArr, closedArr, accentArr);
+});
+
+describe.each`
+  description                                        | original           | sylArr                           | closedArr                      | accentArr
+  ${"4 Syl: 2ms qatal verb consecution (wǝšāmartā́)"} | ${"וְשָׁמַרְתָּ֖"} | ${["וְ", "שָׁ", "מַרְ", "תָּ֖"]} | ${[false, false, true, false]} | ${[false, false, false, true]}
 `("$description", ({ original, sylArr, closedArr, accentArr }) => {
   tests(original, sylArr, closedArr, accentArr);
 });
