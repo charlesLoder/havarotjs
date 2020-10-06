9b15f4abccf1216014da3e8cd2523b442efa2fab
Charles Loder <charlesloder@Charless-MacBook-Pro.local>
2020-10-06 11:31:14 -0400

    added hiriq-yod tests

---
+++ test/mater.test.ts
@@ -0,0 +1,19 @@
+import { Cluster } from "../src/cluster";
+import { Text } from "../src/index";
+
+describe.each`
+  description                     | original     | sylArr                 | isMaterArr
+  ${"hiriq-yod, one syllable"}    | ${"פִּי"}    | ${["פִּי"]}            | ${[false, true]}
+  ${"hiriq-yod, two syllables"}   | ${"קָטִיל"}  | ${["קָ", "טִיל"]}      | ${[false, false, true, false]}
+  ${"hiriq-yod, three syllables"} | ${"מַשִיחַ"} | ${["מַ", "שִי", "חַ"]} | ${[false, false, true, false]}
+`("$description", ({ original, sylArr, isMaterArr }) => {
+  const heb = new Text(original);
+  const sylText = heb.syllables.map((syllable) => syllable.text);
+  const isMater = heb.clusters.map((cluster) => cluster.isMater);
+  test(`syllable text to equal ${sylArr}`, () => {
+    expect(sylText).toEqual(sylArr);
+  });
+  test(`isMater array to equal ${isMaterArr}`, () => {
+    expect(isMater).toEqual(isMaterArr);
+  });
+});
