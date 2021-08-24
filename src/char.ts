f1a29141a63bc1cb48326b44801f6495780e8c13
charlesLoder <charles.w.loder@gmail.com>
2021-08-23 21:34:53 -0400

    Move docs to source

--- src/char.ts
+++ src/char.ts
@@ -4,6 +4,10 @@ const ligature = /[\u{05C1}-\u{05C2}]/u;
 const dagesh = /[\u{05BC},\u{05BF}]/u; // includes rafe
 const niqqud = /[\u{05B0}-\u{05BB},\u{05C7}]/u;
 
+/**
+ * A Hebrew character and its positioning number for being sequenced correctly.
+ * See [[`Cluster`]] for correct normalization.
+ */
 export class Char {
   #text: string;
 
@@ -13,6 +17,12 @@ export class Char {
 
   /**
    * @returns the text of the Char
+   *
+   * ```typescript
+   * const text: Text = new Text("אֱלֹהִ֑ים");
+   * text.chars[0].text;
+   * // "א"
+   * ```
    */
   get text(): string {
     return this.#text;
@@ -40,7 +50,21 @@ export class Char {
   }
 
   /**
-   * @returns the position within a Cluster a type Char should be
+   * @returns a number used for sequencing
+   *
+   * - consonants = 0
+   * - ligatures = 1
+   * - dagesh or rafe = 2
+   * - niqqud (i.e vowels) = 3
+   * - taamim (i.e. accents) = 4
+   *
+   * ```typescript
+   * const text: Text = new Text("אֱלֹהִ֑ים");
+   * text.chars[0].sequencePosition; // the aleph
+   * // 0
+   * text.chars[1].sequencePosition; // the segol
+   * // 3
+   * ```
    */
   get sequencePosition(): number {
     return this.findPos();
--- src/word.ts
+++ src/word.ts
@@ -4,9 +4,66 @@ import { Cluster } from "./cluster";
 import { Char } from "./char";
 import { SylOpts } from "./text";
 
+/**
+ * [[`Text.text`]] is split at each space and maqqef (U+05BE) both of which are captured.
+ * Thus, the string passed to instantiate each `Word` is already properly decomposed, sequenced, qamets qatan patterns converted to the appropriate unicode character (U+05C7), and holem-waw sequences corrected.
+ */
 export class Word {
   #text: string;
+  /**
+   * Returns a string with any whitespace characters (e.g. `/\s/`) from before the word.
+   * It does **not** capture whitespace at the start of a `Text`.
+   *
+   * ```typescript
+   * const heb = `
+   * עֶבֶד
+   * אֱלֹהִים
+   * `;
+   * const text: Text = new Text(heb);
+   * text.words;
+   * // [
+   * //   Word {
+   * //     original: 'עֶבֶד\n',
+   * //     text: 'עֶבֶד',
+   * //     whiteSpaceBefore: '',
+   * //     whiteSpaceAfter: '\n'
+   * //   },
+   * //   Word {
+   * //     original: 'אֱלֹהִים',
+   * //     text: 'אֱלֹהִים',
+   * //     whiteSpaceBefore: '',
+   * //     whiteSpaceAfter: ''
+   * //   }
+   * // ]
+   * ```
+   */
   whiteSpaceBefore: string | null;
+  /**
+   * Returns a string with any whitespace characters (e.g. `/\s/`) after the word.
+   *
+   * ```typescript
+   * const heb = `
+   * עֶבֶד
+   * אֱלֹהִים
+   * `;
+   * const text: Text = new Text(heb);
+   * text.words;
+   * // [
+   * //   Word {
+   * //     original: 'עֶבֶד\n',
+   * //     text: 'עֶבֶד',
+   * //     whiteSpaceBefore: '',
+   * //     whiteSpaceAfter: '\n'
+   * //   },
+   * //   Word {
+   * //     original: 'אֱלֹהִים',
+   * //     text: 'אֱלֹהִים',
+   * //     whiteSpaceBefore: '',
+   * //     whiteSpaceAfter: ''
+   * //   }
+   * // ]
+   * ```
+   */
   whiteSpaceAfter: string | null;
   private sylOpts: SylOpts;
 
@@ -21,6 +78,17 @@ export class Word {
 
   /**
    * @returns the word's text trimmed of any whitespace characters
+   *
+   * ```typescript
+   * const text: Text = new Text("אֵיפֹה־אַתָּה מֹשֶה");
+   * const words = text.words.map((word) => word.text);
+   * words;
+   * // [
+   * //    "אֵיפֹה־",
+   * //    "אַתָּה",
+   * //    "מֹשֶׁה"
+   * //  ]
+   * ```
    */
   get text(): string {
     return this.#text.trim();
@@ -28,6 +96,15 @@ export class Word {
 
   /**
    * @returns a one dimensional array of Syllables
+   *
+   * ```typescript
+   * const text: Text = new Text("אֵיפֹה־אַתָּה מֹשֶה");
+   * text.words[0].syllables;
+   * // [
+   * //    Syllable { original: "אֵי" },
+   * //    Syllable { original: "פֹה־" }
+   * //  ]
+   * ```
    */
   get syllables(): Syllable[] {
     if (/\w/.test(this.text) || this.isDivineName) {
@@ -39,6 +116,17 @@ export class Word {
 
   /**
    * @returns a one dimensional array of Clusters
+   *
+   * ```typescript
+   * const text: Text = new Text("אֵיפֹה־אַתָּה מֹשֶה");
+   * text.words[0].clusters;
+   * // [
+   * //    Cluster { original: "אֵ" },
+   * //    Cluster { original: "י" },
+   * //    Cluster { original: "פֹ" },
+   * //    Cluster { original: "ה־" }
+   * //  ]
+   * ```
    */
   get clusters(): Cluster[] {
     const clusters = makeClusters(this.text);
@@ -50,6 +138,18 @@ export class Word {
 
   /**
    * @returns a one dimensional array of Chars
+   *
+   * ```typescript
+   * const text: Text = new Text("אֵיפֹה־אַתָּה מֹשֶה");text.words[0].chars;
+   * // [
+   * //    Char { original: "א" },
+   * //    Char { original: "ֵ" }, (tsere)
+   * //    Char { original: "פ" },
+   * //    Char { original: "ֹ" }, (holem)
+   * //    Char { original: "ה"},
+   * //    Char { original: "־" }
+   * //  ]
+   * ```
    */
   get chars(): Char[] {
     return this.clusters.map((cluster) => cluster.chars).flat();
@@ -57,6 +157,12 @@ export class Word {
 
   /**
    * @returns a boolean indicating if the text is a form of the Divine Name
+   *
+   * ```typescript
+   * const text: Text = new Text("יְהוָה");
+   * text.words[0].isDivineName;
+   * // true
+   * ```
    */
   get isDivineName(): boolean {
     const nonChars = /[\u{0591}-\u{05C7}]/gu;
