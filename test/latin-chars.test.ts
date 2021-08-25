98c88f020886a8ae0c5ff77c67982d3422151fe4
charlesLoder <charles.w.loder@gmail.com>
2021-08-25 07:11:57 -0400

    Add test for two words and punctuation

--- test/latin-chars.test.ts
+++ test/latin-chars.test.ts
@@ -1,9 +1,10 @@
 import { Text } from "../src/index";
 
 describe.each`
-  description           | original   | numOfSyls | clusterArr               | firstCluster | finalCluster
-  ${"with a comma"}     | ${"עָד,"}  | ${1}      | ${["עָ", "ד", ","]}      | ${"עָ"}      | ${","}
-  ${"with parentheses"} | ${"(אָב)"} | ${1}      | ${["(", "אָ", "ב", ")"]} | ${"("}       | ${")"}
+  description                | original        | numOfSyls | clusterArr                           | firstCluster | finalCluster
+  ${"with a comma"}          | ${"עָד,"}       | ${1}      | ${["עָ", "ד", ","]}                  | ${"עָ"}      | ${","}
+  ${"with parentheses"}      | ${"(אָב)"}      | ${1}      | ${["(", "אָ", "ב", ")"]}             | ${"("}       | ${")"}
+  ${"with a comma, 2 words"} | ${"אָמַר, לֹא"} | ${3}      | ${["אָ", "מַ", "ר", ",", "לֹ", "א"]} | ${"אָ"}      | ${"א"}
 `("Full Vowels:", ({ description, original, numOfSyls, clusterArr, firstCluster, finalCluster }) => {
   const heb = new Text(original);
   const clusterTexts = heb.clusters.map((el) => el.text);
