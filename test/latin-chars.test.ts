b5372e28959051165a47a90341807d15a76a3f15
charlesLoder <charles.w.loder@gmail.com>
2021-08-25 19:46:26 -0400

    Add test for two words w/ parentheses

--- test/latin-chars.test.ts
+++ test/latin-chars.test.ts
@@ -1,10 +1,11 @@
 import { Text } from "../src/index";
 
 describe.each`
-  description                | original        | numOfSyls | clusterArr                           | firstCluster | finalCluster
-  ${"with a comma"}          | ${"עָד,"}       | ${1}      | ${["עָ", "ד", ","]}                  | ${"עָ"}      | ${","}
-  ${"with parentheses"}      | ${"(אָב)"}      | ${1}      | ${["(", "אָ", "ב", ")"]}             | ${"("}       | ${")"}
-  ${"with a comma, 2 words"} | ${"אָמַר, לֹא"} | ${3}      | ${["אָ", "מַ", "ר", ",", "לֹ", "א"]} | ${"אָ"}      | ${"א"}
+  description                    | original         | numOfSyls | clusterArr                                                                                | firstCluster | finalCluster
+  ${"with a comma"}              | ${"עָד,"}        | ${1}      | ${["עָ", "ד", ","]}                                                                       | ${"עָ"}      | ${","}
+  ${"with parentheses"}          | ${"(אָב)"}       | ${1}      | ${["(", "אָ", "ב", ")"]}                                                                  | ${"("}       | ${")"}
+  ${"with a comma, 2 words"}     | ${"אָמַר, לֹא"}  | ${3}      | ${["אָ", "מַ", "ר", ",", "לֹ", "א"]}                                                      | ${"אָ"}      | ${"א"}
+  ${"with parentheses, 2 words"} | ${"(אָמַר לֹא)"} | ${3}      | ${["(", "\u{5D0}\u{5B8}", "\u{5DE}\u{5B7}", "\u{5E8}", "\u{5DC}\u{5B9}", "\u{5D0}", ")"]} | ${"("}       | ${")"}
 `("Full Vowels:", ({ description, original, numOfSyls, clusterArr, firstCluster, finalCluster }) => {
   const heb = new Text(original);
   const clusterTexts = heb.clusters.map((el) => el.text);
