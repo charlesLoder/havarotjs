580bfcc3762779d83e6f753599f7a0b3be2f3e88
Charles Loder <cloder@classicalsubjects.com>
2022-04-02 19:15:43 -0400

    Add check for jerusalem
    - two vowels chars will never appear next to each other in heb text
    - only the spelling of Jersulam should ever have to vowels

--- src/utils/sequence.ts
+++ src/utils/sequence.ts
@@ -6,6 +6,9 @@ import { Char } from "../char";
  */
 export const sequence = (text: string): Char[][] => {
   const splits = /(?=[\u{05C0}\u{05D0}-\u{05F2}])/u;
+  const hiriqPatach = /\u{5B4}\u{5B7}/u;
+  // for Jerusalem, where hiriq precedes patah, replace w/ correct patch-hiriq
+  if (hiriqPatach.test(text)) text = text.replace(hiriqPatach, "\u{5B7}\u{5B4}");
   const clusters = text.split(splits).map((word) => new Cluster(word));
   const sequenced = clusters.map((cluster) => cluster.chars);
   return sequenced;
