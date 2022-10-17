906d5eb1215aff72556743f506f42981730d1685
Charles Loder <cloder@classicalsubjects.com>
2022-10-17 18:57:04 -0400

    Add schemas

---
+++ src/schemas.ts
@@ -0,0 +1,3 @@
+import { SylOpts } from "./text";
+export const traditionalOpts: SylOpts = { qametsQatan: true, sqnmlvy: true, longVowels: true, wawShureq: true };
+export const tiberianOpts: SylOpts = { qametsQatan: false, sqnmlvy: true, longVowels: false, wawShureq: false };
