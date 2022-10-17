21b729e558043e05e25b6dfba0339fd17d70a6ee
Charles Loder <cloder@classicalsubjects.com>
2022-10-17 18:58:33 -0400

    Refactor schemas

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,3 +1,2 @@
 import { SylOpts } from "./text";
-export const traditionalOpts: SylOpts = { qametsQatan: true, sqnmlvy: true, longVowels: true, wawShureq: true };
-export const tiberianOpts: SylOpts = { qametsQatan: false, sqnmlvy: true, longVowels: false, wawShureq: false };
+export const tiberian: SylOpts = { qametsQatan: false, sqnmlvy: true, longVowels: false, wawShureq: false };
