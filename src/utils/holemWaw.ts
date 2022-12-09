6b74275ee097a84e11f5d62b1cd515f5b356e24e
Charles Loder <cloder@classicalsubjects.com>
2022-12-09 15:45:14 -0500

    Refactor for update option

--- src/utils/holemWaw.ts
+++ src/utils/holemWaw.ts
@@ -1,5 +1,23 @@
 import { removeTaamim } from "./removeTaamim";
 import { SylOpts } from "../text";
+import { taamim } from "./regularExpressions";
+
+const findMatches = (word: string, regx: RegExp, cb: (w: string, s: number, e: number) => string) => {
+  regx.lastIndex = 0;
+  const [noTaamim, charPos] = removeTaamim(word);
+  const matches = noTaamim.matchAll(regx);
+  if (!matches) {
+    return word;
+  }
+
+  for (const match of matches) {
+    const start = charPos[match.index!]; // eslint-disable-line
+    const end = charPos[match[0].length] + start;
+    word = cb(word, start, end);
+  }
+  regx.lastIndex = 0;
+  return word;
+};
 
 export const holemWaw = (word: string, options: SylOpts): string => {
   const wawRegX = /\u{05D5}/u;
@@ -7,8 +25,13 @@ export const holemWaw = (word: string, options: SylOpts): string => {
   const holemHaser = /\u{05BA}/u;
   const wawHolemRegX = /\u{05D5}\u{05B9}/u;
   const vowels = /[\u{05B0}-\u{05BB}\u{05C7}]/u;
-  const vowelBeforeWawHolem = new RegExp("(?<!" + vowels.source + ")" + wawHolemRegX.source, "gu");
-
+  /** matches the vav + holem sequence with no vowel before it, indicating that the holem is male */
+  const vavHolemMale = new RegExp("(?<!" + vowels.source + ")" + wawHolemRegX.source, "gu");
+  /** matches the vav + holem sequence with a vowel before it, indicating that the holem is haser */
+  const vavHolemHaser = new RegExp(
+    "(?<=" + vowels.source + ")" + wawRegX.source + taamim.source + "?" + holemRegx.source,
+    "gu"
+  );
   // replace holem haser with regular holem
   if (options.holemHaser === "remove" && holemHaser.test(word)) {
     word = word.replace(holemHaser, "\u{05B9}");
@@ -19,24 +42,24 @@ export const holemWaw = (word: string, options: SylOpts): string => {
     return word;
   }
 
-  const [noTaamim, charPos] = removeTaamim(word);
+  word = vavHolemMale.test(word)
+    ? findMatches(word, vavHolemMale, (word, start, end) => {
+        return (
+          // converts vav + holem as holem male to holem + vav
+          word.substring(0, start) +
+          "\u{05B9}\u{05D5}" +
+          (word.substring(end).trim() ? word.substring(end) : word.substring(end - 1)).replace(holemRegx, "")
+        );
+      })
+    : word;
 
-  // check for waw + holem preceded by vowel
-  // b/c the text is sequenced if there is a taam on the vav
-  // it wouldn't find the pattern
-  const matches = noTaamim.matchAll(vowelBeforeWawHolem);
-  if (!matches) {
-    return word;
-  }
-
-  for (const match of matches) {
-    const start = charPos[match.index!]; // eslint-disable-line
-    const end = charPos[match[0].length] + start;
-    word =
-      word.substring(0, start) +
-      "\u{05B9}\u{05D5}" +
-      (word.substring(end).trim() ? word.substring(end) : word.substring(end - 1)).replace(holemRegx, "");
-  }
+  word =
+    options.holemHaser === "update" && vavHolemHaser.test(removeTaamim(word)[0])
+      ? findMatches(word, vavHolemHaser, (word) => {
+          const vavTaamHolem = new RegExp(`${wawRegX.source}(${taamim.source})?${holemRegx.source}`, "gu");
+          return word.replace(vavTaamHolem, "\u{05D5}\u{05BA}$1");
+        })
+      : word;
 
   return word;
 };
