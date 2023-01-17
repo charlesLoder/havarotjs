905045d6f56ef4a25c61112699dfd72b88213303
Charles Loder <cloder@classicalsubjects.com>
2023-01-17 02:04:38 -0500

    Add vowel name test for syl

--- test/syllable.test.ts
+++ test/syllable.test.ts
@@ -17,3 +17,21 @@ describe.each`
     });
   });
 });
+
+describe.each`
+  description                     | hebrew              | syllableNum | vowelName   | allowNoNiqqud
+  ${"syllable with patach"}       | ${"הַֽ֭יְחָבְרְךָ"} | ${0}        | ${"PATAH"}  | ${false}
+  ${"syllable with shewa"}        | ${"הַֽ֭יְחָבְרְךָ"} | ${1}        | ${"SHEVA"}  | ${false}
+  ${"syllable with silent shewa"} | ${"הַֽ֭יְחָבְרְךָ"} | ${2}        | ${"QAMATS"} | ${false}
+  ${"syllable with none"}         | ${"test"}           | ${0}        | ${null}     | ${true}
+`("vowelName:", ({ description, hebrew, syllableNum, vowelName, allowNoNiqqud }) => {
+  // normally don't use `allowNoNiqqud` in testing, but needed to get `null`
+  const heb = new Text(hebrew, { allowNoNiqqud });
+  const syllable = heb.syllables[syllableNum];
+  const syllableVowelName = syllable.vowelName;
+  describe(description, () => {
+    test(`vowelName to equal ${vowelName}`, () => {
+      expect(syllableVowelName).toEqual(vowelName);
+    });
+  });
+});
