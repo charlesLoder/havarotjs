31696702b1439fadaf95efa2bc3ce34f416c88d6
Charles Loder <cloder@classicalsubjects.com>
2023-01-17 02:04:27 -0500

    Add new test for vowel on syl

--- test/syllable.test.ts
+++ test/syllable.test.ts
@@ -1,10 +1,11 @@
 import { Text } from "../src/index";
 
 describe.each`
-  description               | hebrew              | syllableNum | vowel         | allowNoNiqqud
-  ${"syllable with patach"} | ${"הַֽ֭יְחָבְרְךָ"} | ${0}        | ${"\u{05B7}"} | ${false}
-  ${"syllable with shewa"}  | ${"הַֽ֭יְחָבְרְךָ"} | ${1}        | ${"\u{05B0}"} | ${false}
-  ${"syllable with none"}   | ${"test"}           | ${0}        | ${null}       | ${true}
+  description                     | hebrew              | syllableNum | vowel         | allowNoNiqqud
+  ${"syllable with patach"}       | ${"הַֽ֭יְחָבְרְךָ"} | ${0}        | ${"\u{05B7}"} | ${false}
+  ${"syllable with shewa"}        | ${"הַֽ֭יְחָבְרְךָ"} | ${1}        | ${"\u{05B0}"} | ${false}
+  ${"syllable with silent shewa"} | ${"הַֽ֭יְחָבְרְךָ"} | ${2}        | ${"\u{05B8}"} | ${false}
+  ${"syllable with none"}         | ${"test"}           | ${0}        | ${null}       | ${true}
 `("vowel:", ({ description, hebrew, syllableNum, vowel, allowNoNiqqud }) => {
   // normally don't use `allowNoNiqqud` in testing, but needed to get `null`
   const heb = new Text(hebrew, { allowNoNiqqud });
