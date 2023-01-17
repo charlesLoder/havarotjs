6e1b237d4255435540f48d7a9a1e1f93a9be2a18
Charles Loder <cloder@classicalsubjects.com>
2023-01-17 01:49:10 -0500

    Add new tests

---
+++ test/syllable.test.ts
@@ -0,0 +1,18 @@
+import { Text } from "../src/index";
+
+describe.each`
+  description               | hebrew              | syllableNum | vowel         | allowNoNiqqud
+  ${"syllable with patach"} | ${"הַֽ֭יְחָבְרְךָ"} | ${0}        | ${"\u{05B7}"} | ${false}
+  ${"syllable with shewa"}  | ${"הַֽ֭יְחָבְרְךָ"} | ${1}        | ${"\u{05B0}"} | ${false}
+  ${"syllable with none"}   | ${"test"}           | ${0}        | ${null}       | ${true}
+`("vowel:", ({ description, hebrew, syllableNum, vowel, allowNoNiqqud }) => {
+  // normally don't use `allowNoNiqqud` in testing, but needed to get `null`
+  const heb = new Text(hebrew, { allowNoNiqqud });
+  const syllable = heb.syllables[syllableNum];
+  const syllableVowel = syllable.vowel;
+  describe(description, () => {
+    test(`vowel to equal ${vowel}`, () => {
+      expect(syllableVowel).toEqual(vowel);
+    });
+  });
+});
