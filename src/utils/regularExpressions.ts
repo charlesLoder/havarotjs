5e3e24b2589066729e9ed9412ab4f8e9a717ccc4
Charles Loder <cloder@classicalsubjects.com>
2023-01-17 01:49:28 -0500

    Add new capture group

--- src/utils/regularExpressions.ts
+++ src/utils/regularExpressions.ts
@@ -19,6 +19,15 @@ export const vowels = /[\u{05B1}-\u{05BB}\u{05C7}]/u;
 export const vowelsCaptureGroup = /([\u{05B1}-\u{05BB}\u{05C7}])/u;
 
 /**
+ * a regular expression containing all the Hebrew point characters (excluding rafe) in a capture group
+ *
+ * ```js
+ * /([\u{05B0}-\u{05BB}\u{05C7}])/u;
+ * ```
+ */
+export const vowelsCaptureGroupWithShewa = /([\u{05B0}-\u{05BB}\u{05C7}])/u;
+
+/**
  * a Regex group of the entire Hebrew unicode block including alphabetic presentation forms
  */
 export const hebChars = /[\u{0590}-\u{05FF}\u{FB1D}-\u{FB4F}]/u;
