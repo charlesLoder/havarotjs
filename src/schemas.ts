f84b902548d3158eb40697b07174c1041ee108fb
Charles Loder <cloder@classicalsubjects.com>
2023-06-28 21:28:19 -0400

    Update prop order

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,8 +1,8 @@
 import { SylOpts } from "./text";
 export const tiberian: SylOpts = {
-  qametsQatan: false,
-  sqnmlvy: true,
   longVowels: false,
-  wawShureq: false,
-  shevaAfterMeteg: false
+  qametsQatan: false,
+  shevaAfterMeteg: false,
+  sqnmlvy: true,
+  wawShureq: false
 };
